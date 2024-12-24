5f49a4670ef6318cb63328b706f1b273076420df
FAMCHON Baptiste <bfamchon@gmail.com>
2024-12-24 09:51:08 +0100

    prepare integration testing

--- src/webinars/ports/webinar-repository.interface.ts
+++ src/webinars/ports/webinar-repository.interface.ts
@@ -3,6 +3,5 @@ import { Webinar } from 'src/webinars/entities/webinar.entity';
 export interface IWebinarRepository {
   create(webinar: Webinar): Promise<void>;
   findById(id: string): Promise<Webinar | null>;
-  findByIdSync(id: string): Webinar | null;
   update(webinar: Webinar): Promise<void>;
 }
